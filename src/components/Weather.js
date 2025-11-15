978a2189310be2bef185f7bae1369b0f1d1a456b
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-11-15 23:44:56 +0530

    Created basic component weather and component folder

---
+++ src/components/Weather.js
@@ -0,0 +1,10 @@
+function Weather() {
+  return (
+    <div>
+      <h1>Weather Component</h1>
+      <p>This is a placeholder for the Weather component.</p>
+    </div>
+  );
+}
+
+export default Weather;
